fc8d1059db8c453a004dc1667ecfe58c2d41477e
jamilWebdesign2024 <jamiluddinjishan@gmail.com>
2025-04-20 16:22:11 +0600

    Json Data created

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Auctions from './components/Auctions/Auctions'
 import Banner from './components/Banner/Banner'
 import Footer from './components/Footer/Footer'
 import Navbar from './components/Navbar/Navbar'
@@ -9,6 +10,7 @@ function App() {
     <>
      <Navbar></Navbar>
      <Banner></Banner>
+     <Auctions></Auctions>
      <Footer></Footer>      
     </>
   )
---
+++ src/components/Auctions/Auctions.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Auctions = () => {
+    return (
+        <div className='px-20 py-20' >
+            <h1 className='text-2xl font-semibold mb-4'>Active Auctions</h1>
+            <p>Discover and bid on extraordinary items</p>
+        </div>
+    );
+};
+
+export default Auctions;
